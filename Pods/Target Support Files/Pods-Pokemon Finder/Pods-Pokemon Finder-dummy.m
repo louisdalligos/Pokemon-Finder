e66c3ba614eb8f9337f07d062feee8a338a21075
Louis <Louis@Louiss-MacBook-Air.local>
2016-10-11 15:02:25 +0800

    Install Firebase and Geofire

---
+++ Pods/Target Support Files/Pods-Pokemon Finder/Pods-Pokemon Finder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Pokemon_Finder : NSObject
+@end
+@implementation PodsDummy_Pods_Pokemon_Finder
+@end
---
+++ Pokemon Finder/PokemonFinder-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PokemonFinder-Bridging-Header.h
+//  Pokemon Finder
+//
+//  Created by Louis on 11/10/2016.
+//  Copyright © 2016 Louis. All rights reserved.
+//
+
+
+#import "Geofire.h"
